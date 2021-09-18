622ec101cf0dae82b2d0e13f2bcf18b74498d6e0
Jaume Alemany <jaume@krokis.com>
2021-09-18 23:25:59 +0200

    Do not call `fs.writeFile()` with `undefined` data

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = class EmitAllPlugin {
 
                     compiler.outputFileSystem.writeFile(
                         dest,
-                        source,
+                        source || '',
                         err => {
                             if (err) throw err;
                         }
